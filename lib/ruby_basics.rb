7f537b091f04fc26ddb676780c73d3ec528ee264
Alethea Cary <aletheacary@gmail.com>
2019-05-06 01:32:07 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -4,19 +4,19 @@ def division(num1, num2)
 end
 
 def assign_variable(value)
-  
+  "#{value}"
 end
 
-def argue
-  
+def argue (argument)
+  "#{argument}""
 end
 
-def greeting
-
+def greeting (greeting, name)
+puts "#{greeting} #{name}!"
 end
 
 def return_a_value
-
+  return_a_value="Nice"
 end
 
 def last_evaluated_value
